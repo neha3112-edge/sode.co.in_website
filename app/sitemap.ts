cb785cf30d5cba34da88024b2f5a76a7fa1073d2
abhijadon <abhisheksode001@gmail.com>
2026-07-15 16:15:51 +0530

    sdf

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,19 +7,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}/`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: `${baseUrl}/iiitb`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.9,
     },
     {
       url: `${baseUrl}/iimk`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.9,
     },
   ];
